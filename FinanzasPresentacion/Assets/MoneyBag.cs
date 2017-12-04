0e5c9c26a1f533f801d3756e0cd4c0f31cc11e2f
serlin29894 <change29894@gmail.com>
2017-12-04 17:56:28 +0100

    sergi

--- FinanzasPresentacion/Assets/MoneyBag.cs
+++ FinanzasPresentacion/Assets/MoneyBag.cs
@@ -4,14 +4,37 @@ using UnityEngine;
 
 public class MoneyBag : MonoBehaviour {
 
+    public GameObject anim;
+
+    private bool Taken;
+
+    private float timea;
+    private float tima = 0;
+
+    public void Update()
+    {
+        if (Taken)
+        {
+            tima += Time.deltaTime;
+            if (tima > 0.2)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
 
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
-            Destroy(this.gameObject);
+            anim.SetActive(true);
+            transform.GetComponent<SpriteRenderer>().enabled = false;
+            anim.GetComponent<Animator>().SetBool("isTaken", true);
         }
+
+        Taken = true;
+
     }
 
 
